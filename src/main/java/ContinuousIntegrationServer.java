7cf4db6ac537a5f3e0c61a9f349e9c9395c2acfa
asta <astaol@kth.se>
2022-02-11 08:39:52 +0100

    failed commit

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -33,7 +33,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         Server server = new Server(8080);
         server.setHandler(new ContinuousIntegrationServer());
         server.start();
-        server.join();
+        server.join()
     }
 
     /**
